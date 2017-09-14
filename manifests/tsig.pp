26110228b00251c630dffb793d4cafec945f2829
Simon Deziel <simon.deziel@gmail.com>
2017-09-14 11:43:05 -0400

    nsd: add tsig define

---
+++ manifests/tsig.pp
@@ -0,0 +1,29 @@
+# Define: nsd::tsig
+#
+# Parameters:
+#
+# $ensure = ensure the persence or absence of the tsig key.
+# $keyname = the name given to the TSIG KEY. This must be unique. This defaults to
+#   the namevar.
+# $algorithm = Defined algorithm of the key
+# $secret = shared secret of the key
+#
+# Usage:
+#
+# nsd::tsig { 'ns3':
+#   algorithm => 'hmac-sha512'
+#   secret    => 'Kmx0RGasejvesXopWfOPVQ==',
+# }
+#
+define nsd::tsig (
+  String $secret,
+  String $algorithm                = 'hmac-sha512',
+  Enum['present','absent'] $ensure = present
+) {
+  file { "${nsd::tsig_dir}/${name}.conf":
+    owner   => 0,
+    group   => 'nsd',
+    mode    => '0640',
+    content => epp('nsd/tsig.epp', { 'name' => $name, 'algorithm' => $algorithm, 'secret' => $secret }),
+  }
+}
