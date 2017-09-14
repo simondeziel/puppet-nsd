d91900ad45660b38fa651efddeb9b7994856d7c9
Simon Deziel <simon.deziel@gmail.com>
2017-09-14 15:58:41 -0400

    nsd: add zone define

---
+++ manifests/zone.pp
@@ -0,0 +1,36 @@
+# Define: nsd::zone
+#
+define nsd::zone (
+  Optional[Hash] $options          = undef,
+  Enum['absent','present'] $ensure = 'present',
+  Optional[String] $content        = undef,
+  Optional[String] $source         = undef,
+) {
+  if $ensure == 'present' and ! $options {
+    fail('nsd::zone requires the options hash when the zone is present')
+  }
+
+  # content or source are used for "master" zones
+  # which shouldn't be writeable by nsd
+  if $content or $source {
+    $mode = '0440'
+  } else {
+    $mode = '0640'
+  }
+  
+  # the zone file itself
+  file { "${nsd::zones_dir}/${name}":
+    ensure  => $ensure,
+    content => $content,
+    source  => $source,
+    owner   => 'nsd',
+    group   => 'nsd',
+    mode    => $mode,
+  }
+
+  # the config file for that zone
+  file { "${nsd::cfg_dir}/${name}.zone":
+    ensure  => $ensure,
+    content => epp('nsd/zone.epp', { 'name' => $name, 'options' => $options }),
+  }
+}
