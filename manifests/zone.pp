ac7d6f118cd50e0afd176d65f522dd43990332cc
Simon Deziel <simon@sdeziel.info>
2017-09-19 13:43:38 -0400

    nsd::zone: improve handling of absent, slave and master zones

--- manifests/zone.pp
+++ manifests/zone.pp
@@ -12,31 +12,50 @@ define nsd::zone (
     fail('nsd::zone requires the options hash when the zone is present')
   }
 
-  # content or source are used for "master" zones
-  # which shouldn't be writeable by nsd
-  if $content or $source {
-    $owner = 0
+  # zone file
+  if $ensure == 'absent' {
+    # when removing a zone file, no need to reload
+    # this will be done by the associated config file
+    file { "${nsd::zones_dir}/${name}":
+      ensure => $ensure,
+      backup => $backup_zone,
+    }
+  } elsif $content or $source {
+    # content or source indicate a "master" zone
+    # shouldn't be writable by nsd (hence owner => 0)
+    file { "${nsd::zones_dir}/${name}":
+      ensure       => file,
+      content      => $content,
+      source       => $source,
+      owner        => 0,
+      group        => 'nsd',
+      mode         => '0644',
+      validate_cmd => $checkzone_cmd,
+      backup       => $backup_zone,
+      notify       => Exec['nsd-reload'],
+    }
   } else {
-    $owner = 'nsd'
-  }
-  
-  # the zone file itself
-  file { "${nsd::zones_dir}/${name}":
-    ensure       => $ensure,
-    content      => $content,
-    source       => $source,
-    owner        => $owner,
-    group        => 'nsd',
-    mode         => '0644',
-    validate_cmd => $checkzone_cmd,
-    backup       => $backup_zone,
-    notify       => Exec['nsd-reload'],
+    # otherwise it's a slave zone for which the file
+    # shouldn't be managed (hence the ensure => undef)
+    # nsd will write those itself at periodic intervals
+    file { "${nsd::zones_dir}/${name}":
+      ensure => undef,
+      owner  => 'nsd',
+      group  => 'nsd',
+      mode   => '0644',
+    }
   }
 
-  # the config file for that zone
-  nsd::conf { "zone-${name}.conf":
-    ensure  => $ensure,
-    content => epp('nsd/zone.epp', { 'name' => $name, 'options' => $options }),
-    require => File["${nsd::zones_dir}/${name}"],
+  # config file associated with the zone
+  if $ensure == 'present' {
+    nsd::conf { "zone-${name}.conf":
+      ensure  => $ensure,
+      content => epp('nsd/zone.epp', { 'name' => $name, 'options' => $options }),
+      require => File["${nsd::zones_dir}/${name}"],
+    }
+  } else {
+    nsd::conf { "zone-${name}.conf":
+      ensure  => $ensure,
+    }
   }
 }
