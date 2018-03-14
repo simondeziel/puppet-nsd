ad3bf10a16901a2232eb4faba5d1f909d7beab1c
Simon Deziel <simon@sdeziel.info>
2018-03-14 16:11:35 -0400

    nsd::zone: support unmanaged zonefile

--- manifests/zone.pp
+++ manifests/zone.pp
@@ -5,6 +5,7 @@ define nsd::zone (
   Enum['absent','present'] $ensure     = 'present',
   Optional[String] $content            = undef,
   Optional[String] $source             = undef,
+  Boolean $manage_zonefile             = true,
   Optional[String] $checkzone_cmd      = $nsd::checkzone_cmd,
   Variant[Boolean,String] $backup_zone = $nsd::backup_zone,
 ) {
@@ -16,43 +17,45 @@ define nsd::zone (
   $zonefile = "${nsd::zones_dir}/${zonename}"
 
   # zone file
-  if $ensure == 'absent' {
-    # when removing a zone file, no need to reload
-    # this will be done by the associated config file
-    file { $zonefile:
-      ensure => $ensure,
-      backup => $backup_zone,
-    }
-  } elsif $content or $source {
+  if $manage_zonefile {
+    if $ensure == 'absent' {
+      # when removing a zone file, no need to reload
+      # this will be done by the associated config file
+      file { $zonefile:
+        ensure => $ensure,
+        backup => $backup_zone,
+      }
+    } elsif $content or $source {
 
-    if $checkzone_cmd {
-      $validate_cmd = "${checkzone_cmd} ${name} %"
+      if $checkzone_cmd {
+        $validate_cmd = "${checkzone_cmd} ${name} %"
+      } else {
+        $validate_cmd = undef
+      }
+
+      # content or source indicate a "master" zone
+      # shouldn't be writable by nsd (hence owner => 0)
+      file { $zonefile:
+        ensure       => file,
+        content      => $content,
+        source       => $source,
+        owner        => 0,
+        group        => 'nsd',
+        mode         => '0644',
+        validate_cmd => $validate_cmd,
+        backup       => $backup_zone,
+        notify       => Exec['nsd-reload'],
+      }
     } else {
-      $validate_cmd = undef
-    }
-
-    # content or source indicate a "master" zone
-    # shouldn't be writable by nsd (hence owner => 0)
-    file { $zonefile:
-      ensure       => file,
-      content      => $content,
-      source       => $source,
-      owner        => 0,
-      group        => 'nsd',
-      mode         => '0644',
-      validate_cmd => $validate_cmd,
-      backup       => $backup_zone,
-      notify       => Exec['nsd-reload'],
-    }
-  } else {
-    # otherwise it's a slave zone for which the file
-    # shouldn't be managed (hence the ensure => undef)
-    # nsd will write those itself at periodic intervals
-    file { $zonefile:
-      ensure => undef,
-      owner  => 'nsd',
-      group  => 'nsd',
-      mode   => '0644',
+      # otherwise it's a slave zone for which the file
+      # shouldn't be managed (hence the ensure => undef)
+      # nsd will write those itself at periodic intervals
+      file { $zonefile:
+        ensure => undef,
+        owner  => 'nsd',
+        group  => 'nsd',
+        mode   => '0644',
+      }
     }
   }
 
